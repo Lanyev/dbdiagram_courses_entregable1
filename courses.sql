10b860db39560ce559dfcced26197cc7c63a464a
Alan Yeverino <lanyev@gmail.com>
2023-01-24 19:11:30 -0600

    final update.... i think

--- courses.sql
+++ courses.sql
@@ -1,86 +1,61 @@
+CREATE TABLE "users" (
+  "id" serial PRIMARY KEY,
+  "first_name" varchar NOT NULL,
+  "last_name" varchar NOT NULL,
+  "email" varchar NOT NULL UNIQUE,
+  "password" varchar NOT NULL,
+  "age" int NOT NULL
+);
+
+CREATE TABLE "teachers" (
+  "id" serial PRIMARY KEY,
+  "first_name" varchar NOT NULL,
+  "last_name" varchar NOT NULL,
+  "img_url" varchar NOT NULL
+);
+
+CREATE TABLE "courses" (
+  "id" serial PRIMARY KEY,
+  "teacher_id" int NOT NULL,
+  "category_id" int NOT NULL,
+  "level_id" int NOT NULL,
+  "title" varchar NOT NULL,
+  "description" varchar NOT NULL
+);
+
+CREATE TABLE "users_courses" (
+  "id" serial PRIMARY KEY,
+  "user_id" int NOT NULL,
+  "course_id" int NOT NULL
+);
+
 CREATE TABLE "course_videos" (
-  "id" uuid PRIMARY KEY,
-  "course_id" uuid NOT NULL,
+  "id" serial PRIMARY KEY,
+  "course_id" int NOT NULL,
   "title" varchar NOT NULL,
   "duration" time NOT NULL,
   "video_url" varchar NOT NULL,
   "img_url" varchar NOT NULL
 );
 
-CREATE TABLE "courses" (
-  "id" uuid PRIMARY KEY,
-  "teacher_id" uuid NOT NULL,
-  "level_id" uuid NOT NULL,
-  "title" varchar NOT NULL,
-  "description" varchar NOT NULL
-);
-
 CREATE TABLE "categories" (
-  "id" uuid PRIMARY KEY,
+  "id" serial PRIMARY KEY,
   "name" varchar NOT NULL
-);
-
-CREATE TABLE "categories_courses" (
-  "id" uuid PRIMARY KEY,
-  "category_id" uuid NOT NULL,
-  "course_id" uuid NOT NULL
-);
-
-CREATE TABLE "users" (
-  "id" uuid PRIMARY KEY,
-  "first_name" varchar NOT NULL,
-  "last_name" varchar NOT NULL,
-  "email" varchar NOT NULL,
-  "password" varchar NOT NULL,
-  "age" int
-);
-
-CREATE TABLE "users_courses" (
-  "id" uuid PRIMARY KEY,
-  "user_id" uuid NOT NULL,
-  "course_id" uuid NOT NULL
-);
-
-CREATE TABLE "teachers" (
-  "id" uuid PRIMARY KEY,
-  "first_name" varchar NOT NULL,
-  "last_name" varchar NOT NULL,
-  "img_url" varchar NOT NULL
 );
 
 CREATE TABLE "levels" (
-  "id" uuid PRIMARY KEY,
+  "id" serial PRIMARY KEY,
   "name" varchar NOT NULL
 );
 
-CREATE TABLE "reviews" (
-  "id" uuid PRIMARY KEY,
-  "user_id" uuid NOT NULL,
-  "course_id" uuid NOT NULL,
-  "rating" int NOT NULL,
-  "comment" varchar NOT NULL
-);
-
-ALTER TABLE "users_courses" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
-
 ALTER TABLE "course_videos" ADD FOREIGN KEY ("course_id") REFERENCES "courses" ("id");
-
-ALTER TABLE "categories_courses" ADD FOREIGN KEY ("category_id") REFERENCES "courses" ("id");
-
-ALTER TABLE "categories_courses" ADD FOREIGN KEY ("course_id") REFERENCES "categories" ("id");
-
-ALTER TABLE "courses" ADD FOREIGN KEY ("teacher_id") REFERENCES "teachers" ("id");
-
-ALTER TABLE "categories_courses" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
 
 ALTER TABLE "courses" ADD FOREIGN KEY ("level_id") REFERENCES "levels" ("id");
 
-ALTER TABLE "reviews" ADD FOREIGN KEY ("course_id") REFERENCES "courses" ("id");
-
-ALTER TABLE "reviews" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
-
 ALTER TABLE "users_courses" ADD FOREIGN KEY ("course_id") REFERENCES "courses" ("id");
 
-ALTER TABLE "categories_courses" ADD FOREIGN KEY ("course_id") REFERENCES "courses" ("id");
+ALTER TABLE "courses" ADD FOREIGN KEY ("teacher_id") REFERENCES "teachers" ("id");
 
+ALTER TABLE "users_courses" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
+ALTER TABLE "courses" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
--- scripts.sql
+++ scripts.sql
@@ -1,210 +1,58 @@
-insert into categories 
-(name)
+INSERT INTO "categories" ("name")
+VALUES ('Programming'),
+('Design'),
+('Business'),
+('Marketing'),
+('Education');
 
-values 
-('Web Development'),
-('Programming Languages');
-
-inser into levels 
-(name)
-
-values 
-('Beginner'),
+INSERT INTO "levels" ("name")
+VALUES ('Beginner'),
 ('Intermediate'),
 ('Advanced');
 
-insert into teachers
-(first_name, last_name, img_url)
+INSERT INTO "teachers" ("first_name", "last_name", "img_url")
+VALUES ('Juan', 'Perez', 'https://alancourses.com/imagepf1'),
+('Maria', 'Gomez', 'https://alancourses.com/imagepf2'),
+('Pedro', 'Gonzalez', 'https://alancourses.com/imagepf3'),
+('Ana', 'Lopez', 'https://alancourses.com/imagepf4'),
+('Luis', 'Garcia', 'https://alancourses.com/imagepf5');
 
-values 
-('Juan', 'Perez' 'juanperez@alancourses.com'),
-('Maria', 'Gomez', 'mariagomez@alancourses.com'),
-('Pedro', 'Gonzalez', 'pedrogonzalez@alancourses.com'),
-('Jose', 'Martinez', 'josemartinedz@alancourses.com'),
-('Luis', 'Rodriguez', 'luisrodriguez@alancourses.com'),
-('Ana', 'Lopez', 'analopez@alancourses.com'),
-('Sofia', 'Gutierrez', 'sofiagutierrez@alancourses.com'),
-('Miguel', 'Hernandez', 'miguelhernandez@alancourses.com');
+INSERT INTO "courses" ("teacher_id", "category_id", "level_id", "title", "description")
+VALUES (1, 1, 1, 'Introduction to Programming', 'Learn the basics of programming in this beginner-friendly course'),
+(2, 2, 2, 'Intermediate Graphic Design', 'Take your design skills to the next level with this intermediate course'),
+(3, 3, 3, 'Advanced Business Strategy', 'Learn advanced business strategies to take your company to the next level'),
+(4, 4, 2, 'Intermediate Digital Marketing', 'Learn the latest digital marketing techniques to promote your brand'),
+(5, 5, 3, 'Advanced Education Theory', 'Explore advanced education theories and principles to improve your teaching skills');
 
-insert into courses
-(teacher_id, level_id, title, description)
+INSERT INTO "users" ("first_name", "last_name", "email", "password", "age")
+VALUES ('Pablo', 'Perez', 'pabloperez@correo.com', '123456', 22),
+('Maria', 'Garcia', 'mariagarcia@correo.com', '123456', 25),
+('Juan', 'Rodriguez', 'juanrodriguez@correo.com', '123456', 30),
+('Ana', 'Martinez', 'anamartinez@correo.com', '123456', 35),
+('Luis', 'Gonzalez', 'luisgonzalez@correo.com', '123456', 40),
+('Carlos', 'Sanchez', 'carlossanchez@correo.com', '123456', 45),
+('Rosa', 'Diaz', 'rosadiaz@correo.com', '123456', 50),
+('Jorge', 'Lopez', 'jorgelopez@correo.com', '123456', 55),
+('Carmen', 'Perez', 'carmenperez@correo.com', '123456', 60),
+('Javier', 'Garcia', 'javiergarcia@correo.com', '123456', 65),
+('Andres', 'Martinez', 'andresmartinez@correo.com', '123456', 70);
 
-values
-('1', '1', 'HTML5', 'HTML5 is the latest version of HTML, the markup language used to create web pages. HTML5 is a markup language used for structuring and presenting content on the World Wide Web. It is the fifth and current major version of the HTML standard, and subsumes XHTML.'),
+INSERT INTO "users_courses" ("user_id", "course_id")
+VALUES (1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 1),
+(7, 2),
+(8, 3),
+(9, 4),
+(10, 5),
+(11, 1);
 
-('2', '1', 'CSS3', 'CSS3 is the latest evolution of the Cascading Style Sheets language and aims at extending CSS2.1. Many of these new features are already being implemented in many major browsers. The module defines new values for many of the properties defined in CSS2.1, and adds new properties.'),
-
-('3', '1', 'JavaScript', 'JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. — you can bet that JavaScript is probably involved.'),
-
-('4', '1', 'PHP', 'PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages. PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft\'s ASP.'),
-
-('5', '1', 'Python', 'Python is a programming language that lets you work more quickly and integrate your systems more effectively. You can learn to use Python and see almost immediate gains in productivity and lower maintenance costs.'),
-
-('6', '1', 'Java', 'Java is a general-purpose computer-programming language that is concurrent, class-based, object-oriented, and specifically designed to have as few implementation dependencies as possible. It is intended to let application developers "write once, run anywhere" (WORA), meaning that compiled Java code can run on all platforms that support Java without the need for recompilation.'),
-
-('7', '1', 'C++', 'C++ is a general-purpose programming language. It has imperative, object-oriented and generic programming features, while also providing facilities for low-level memory manipulation. It was designed with a bias toward system programming and embedded, resource-constrained and large systems, with performance, efficiency and flexibility of use as its design highlights.'),
-
-('8', '1', 'C#', 'C# is a general-purpose, multi-paradigm programming language encompassing strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented (class-based), and component-oriented programming disciplines. It was developed around 2000 by Microsoft within its .NET initiative and later approved as a standard by Ecma (ECMA-334) and ISO (ISO/IEC 23270:2018). C# is one of the programming languages designed for the Common Language Infrastructure.');
-
-
-insert into course_videos 
-(id_course, title, duration, video_url, portada_url)
-
-values 
-('1', 'HTML5', '1:30', 'https://alancourses.com/watchmyvideo1', 'https://alancourses.com/image1'),
-('2', 'CSS3', '1:30', 'https://alancourses.com/watchmyvideo2', 'https://alancourses.com/image2'),
-('3', 'JavaScript', '1:30', 'https://alancourses.com/watchmyvideo3', 'https://alancourses.com/image3'),
-('4', 'PHP', '1:30', 'https://alancourses.com/watchmyvideo4', 'https://alancourses.com/image4'),
-('5', 'Python', '1:30', 'https://alancourses.com/watchmyvideo5', 'https://alancourses.com/image5'),
-('6', 'Java', '1:30', 'https://alancourses.com/watchmyvideo6', 'https://alancourses.com/image6'),
-('7', 'C++', '1:30', 'https://alancourses.com/watchmyvideo7', 'https://alancourses.com/image7'),
-('8', 'C#', '1:30', 'https://alancourses.com/watchmyvideo8', 'https://alancourses.com/image8');
-
-
-insert into categories_courses
-(id_category, id_course)
-
-values 
-('1', '1'),
-('1', '2'),
-('1', '3'),
-('1', '4'),
-('1', '5'),
-('1', '6'),
-('1', '7'),
-('1', '8'),
-
-('2', '1'),
-('2', '2'),
-('2', '3'),
-('2', '4'),
-('2', '5'),
-('2', '6'),
-('2', '7'),
-('2', '8');
-
-insert into users
-(first_name, last_name, email, password, img_url)
-
-values 
-('Pablo', 'Perez', 'pabloperez@correo.com', '123456', 'https://alancourses.com/image1'),
-('Juan', 'Garcia', 'juangarcia@correo.com', '654321', 'https://alancourses.com/image2'),
-('Maria', 'Rodriguez', 'mariarodriguez@correo.com', '789012', 'https://alancourses.com/image3'),
-('Carlos', 'Sanchez', 'carlossanchez@correo.com', '345678', 'https://alancourses.com/image4'),
-('Ana', 'Martinez', 'anamartinez@correo.com', '901234', 'https://alancourses.com/image5'),
-('Luis', 'Gonzalez', 'luisgonzalez@correo.com', '567890', 'https://alancourses.com/image6'),
-('Pedro', 'Jimenez', 'pedrojimenez@correo.com', '012345', 'https://alancourses.com/image7'),
-('Isabel', 'Lopez', 'isabellopez@correo.com', '678901', 'https://alancourses.com/image8'),
-('Sofia', 'Perez', 'sofiaperez@correo.com', '234567', 'https://alancourses.com/image9'),
-('Alejandro', 'Garcia', 'alejandrogarcia@correo.com', '890123', 'https://alancourses.com/image10'),
-('Jorge', 'Rodriguez', 'jorgerodriguez@correo.com', '456789', 'https://alancourses.com/image11');
-
-insert into users_courses
-(id_user, id_course)
-
-values 
-('1', '1'),
-('1', '2'),
-('1', '3'),
-
-('2', '1'),
-('2', '2'),
-('2', '3'),
-
-('3', '1'),
-('3', '2'),
-('3', '3'),
-
-('4', '1'),
-('4', '2'),
-('4', '3'),
-
-('5', '1'),
-('5', '2'),
-('5', '3'),
-
-('6', '1'),
-('6', '2'),
-('6', '3'),
-
-('7', '1'),
-('7', '2'),
-('7', '3'),
-
-('8', '1'),
-('8', '2'),
-('8', '3'),
-
-('9', '1'),
-('9', '2'),
-('9', '3'),
-
-('10', '1'),
-('10', '2'),
-('10', '3'),
-
-('11', '1'),
-('11', '2'),
-('11', '3');
-
-insert into reviews 
-(id_user, id_course, review, rating)
-
-values 
-//add 1 review per user depending on the course they have taken
-
-('1', '1', 'Excelente curso, muy recomendado', '5'),
-('1', '2', 'Excelente curso, muy recomendado', '5'),
-('1', '3', 'Excelente curso, muy recomendado', '5'),
-
-('2', '1', 'Excelente curso, muy recomendado', '5'),
-('2', '2', 'Excelente curso, muy recomendado', '5'),
-('2', '3', 'Excelente curso, muy recomendado', '5'),
-
-('3', '1', 'Excelente curso, muy recomendado', '5'),
-('3', '2', 'Excelente curso, muy recomendado', '5'),
-('3', '3', 'Excelente curso, muy recomendado', '5'),
-
-('4', '1', 'Excelente curso, muy recomendado', '5'),
-('4', '2', 'Excelente curso, muy recomendado', '5'),
-('4', '3', 'Excelente curso, muy recomendado', '5'),
-
-('5', '1', 'Excelente curso, muy recomendado', '5'),
-('5', '2', 'Excelente curso, muy recomendado', '5'),
-('5', '3', 'Excelente curso, muy recomendado', '5'),
-
-('6', '1', 'Excelente curso, muy recomendado', '5'),
-('6', '2', 'Excelente curso, muy recomendado', '5'),
-('6', '3', 'Excelente curso, muy recomendado', '5'),
-
-('7', '1', 'Excelente curso, muy recomendado', '5'),
-('7', '2', 'Excelente curso, muy recomendado', '5'),
-('7', '3', 'Excelente curso, muy recomendado', '5'),
-
-('8', '1', 'Excelente curso, muy recomendado', '5'),
-('8', '2', 'Excelente curso, muy recomendado', '5'),
-('8', '3', 'Excelente curso, muy recomendado', '5'),
-
-('9', '1', 'Excelente curso, muy recomendado', '5'),
-('9', '2', 'Excelente curso, muy recomendado', '5'),
-('9', '3', 'Excelente curso, muy recomendado', '5'),
-
-('10', '1', 'Excelente curso, muy recomendado', '5'),
-('10', '2', 'Excelente curso, muy recomendado', '5'),
-('10', '3', 'Excelente curso, muy recomendado', '5'),
-
-('11', '1', 'Excelente curso, muy recomendado', '5'),
-('11', '2', 'Excelente curso, muy recomendado', '5'),
-('11', '3', 'Excelente curso, muy recomendado', '5');
-
-
-
-
-
-
-
-
-
-
-
-
+INSERT INTO "course_videos" ("course_id", "title", "duration", "video_url", "img_url")
+VALUES (1, 'Lesson 1: Introduction to Variables', '00:30:00', 'https://alancourses.com/video1', 'https://alancourses.com/image1'),
+(1, 'Lesson 2: Conditional Statements', '00:40:00', 'https://alancourses.com/video2', 'https://alancourses.com/image2'),
+(1, 'Lesson 3: Loops', '00:45:00', 'https://alancourses.com/video3', 'https://alancourses.com/image3'),
+(2, 'Lesson 1: Color Theory', '00:35:00', 'https://alancourses.com/video4', 'https://alancourses.com/image4'),
+(2, 'Lesson 2: Typography', '00:45:00', 'https://alancourses.com/video5', 'https://alancourses.com/image5');
